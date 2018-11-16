5719a18f7b1274ec86672e38c5e10938bc28c8a5
Heba <heba.elliethy1990@gmail.com>
2018-11-16 10:26:48 +0100

    Adding the Searchterm and limit

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,25 @@ var router = express.Router();
 const trending = require('trending-github');
 
 router.get('/trending', function (req, res, next) {
- 
+  var searchterm = req.query.searchterm;
+  var limit = req.query.limit;
+
   trending()
     .then(repos => {
-      var stars = repos.map(repo => repo.stars)
-      var sumStars = stars.reduce(add, 0);
-      res.send({ "Repositry": repos, "SumStars": sumStars })
+      var filteredRepos = filterItems(repos, searchterm);
+      var limitedRepos = filteredRepos.splice(0, limit);
+      var star = limitedRepos.map(repo => repo.stars)
+      var sumStars = star.reduce(add, 0);
+      res.send({ "Repositry": limitedRepos, "SumStars": sumStars })
     });
-
-
 });
 function add(a, b) {
   return a + b;
 }
+
+function filterItems(repos, searchterm) {
+  return repos.filter(repo => repo.name.toLowerCase().indexOf(searchterm.toLowerCase()) > -1);
+}
+
 
 module.exports = router;
