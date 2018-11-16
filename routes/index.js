b881b78a172e914c85d6b242d6ce2002affa3da9
Heba <heba.elliethy1990@gmail.com>
2018-11-16 10:11:27 +0100

    Get the github repository

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const trending = require('trending-github');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/trending', function (req, res, next) {
+ 
+  trending()
+    .then(repos => {
+      var stars = repos.map(repo => repo.stars)
+      var sumStars = stars.reduce(add, 0);
+      res.send({ "Repositry": repos, "SumStars": sumStars })
+    });
+
+
 });
+function add(a, b) {
+  return a + b;
+}
 
 module.exports = router;
